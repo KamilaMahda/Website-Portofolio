a57ae3f32e1a068fd9be9703eed8d7fce941bcaf
KamilaMahda <541241101@student.smktelkom-pwt.sch.id>
2024-11-06 14:26:19 +0700

    Add files via upload

---
+++ Looping_while.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class demo_while1
+{
+    static void Main()
+    {
+        Console.Clear();
+        int i;
+        i = 1; //nilai awal, preparation
+        while (i <= 5) // nilai akhir, decision
+        {
+            Console.WriteLine(i + " ");
+            i = i + 1;//counter, proses
+        }
+    }
+}
